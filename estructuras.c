5a47699e68c916286db77b8410c8dd9b6b7a0fad
isRa <99518333+isRa-Gtb@users.noreply.github.com>
2024-02-22 20:33:13 -0600

    Create estructuras.c

---
+++ estructuras.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#define s scanf
+#define p printf
+
+int main(){
+}
+
+
+
+
+
+struct libro {
+char titulo;
+char editorial;
+char genero;
+int paginas;
+char fecha;
+char *autores[3];
+}
+
